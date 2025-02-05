0ae6c44eb72686096f6072f273a69b9e1b4feb47
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-02-04 22:58:43 -0600

    credentials

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+rails db:{migrate,seed}
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+# bundle exec rails db:migrate
